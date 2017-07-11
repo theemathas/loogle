1368c7a397d22c52f2bc42e1cc43bec4d9e2b7b1
Theemathas Chirananthavat <theemathas@gmail.com>
2017-07-10 21:30:19 -0700

    Use accumulation for unify_with_args

--- main.lean
+++ main.lean
@@ -12,44 +12,39 @@ meta def tactic.will_succeed {α : Type u} (t : tactic α) : tactic bool :=
       end
     in result.success b ts
 
-/-
-Replace all metavariables in an expr with fresh metavariables
-Multiple occurrences of a single metavariable gets replaced by a single fresh metavariable
--/
-meta def tactic.replace_fresh_mvars (e : expr) : tactic expr :=
-  -- TODO
-  return e
+/- Get the return value of a tactic without its side effects -/
+meta def tactic.value_only {α : Type u} (t : tactic α) : tactic α :=
+  λ ts,
+    match t ts with
+    | (result.success val _) := result.success val ts
+    | (result.exception msg ref _) := result.exception msg ref ts
+    end
 
 /- Helper function for unify_with_args -/
-private meta def unify_with_args_inner : expr → expr → tactic (option (list expr)) := λ fixed_type add_args_type, do
-can_unify ← tactic.will_succeed $ tactic.unify fixed_type add_args_type,
-if can_unify then do
-  tactic.unify fixed_type add_args_type,
-  return $ some []
-else
-  match add_args_type with
-  | (expr.pi arg_name _ arg_type body) := do
-      arg_mvar ← tactic.mk_meta_var arg_type,
-      let substituted_expr := body.instantiate_var arg_mvar,
-      recur_result ← unify_with_args_inner fixed_type substituted_expr,
-      match recur_result with
-      | some arg_types_rest := do
-          unified_arg_type ← tactic.instantiate_mvars arg_type,
-          return $ some $ unified_arg_type :: arg_types_rest
-      | none := return none
-      end
-  | _ := return none
-  end
+private meta def unify_with_args_inner : expr → expr → list expr → tactic (option (list expr)) :=
+λ fixed_type add_args_type accum_arg_types, do
+  can_unify ← tactic.will_succeed $ tactic.unify fixed_type add_args_type,
+  if can_unify then
+    tactic.value_only $ do
+      tactic.unify fixed_type add_args_type,
+      substituted_arg_types ← monad.sequence $ accum_arg_types.reverse.map tactic.instantiate_mvars,
+      return $ some substituted_arg_types
+  else
+    match add_args_type with
+    | (expr.pi arg_name _ arg_type body) := do
+        arg_mvar ← tactic.mk_meta_var arg_type,
+        let substituted_expr := body.instantiate_var arg_mvar,
+        unify_with_args_inner fixed_type substituted_expr (arg_type :: accum_arg_types)
+    | _ := return none
+    end
 
 /-
 Add arguments to `add_args_type` so it unifies with `fixed_type`
 Returns `some [T1, T2, ..., Tn]` if unifiable after putting in arguments of types T1, T2, ..., Tn
 Returns `none` if fails to unify
 -/
-meta def unify_with_args (fixed_type add_args_type : expr) : tactic (option (list expr)) := do
-  fixed_type' ← tactic.replace_fresh_mvars fixed_type,
-  add_args_type' ← tactic.replace_fresh_mvars add_args_type,
-  unify_with_args_inner fixed_type' add_args_type'
+meta def unify_with_args (fixed_type add_args_type : expr) : tactic (option (list expr)) :=
+  unify_with_args_inner fixed_type add_args_type []
 
 @[hole_command]
 meta def any_value_that_fits : hole_command :=
